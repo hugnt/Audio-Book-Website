596219d5d4445ab284156c54e57e54cbcbfad495
hugnt <thanhhungst314@gmail.com>
2023-03-03 10:07:48 +0700

    2nd push

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../admin/controllers/AdminController');
-
+const audioBookAdmin = require('../audioBookAdmin');
 router.get('/', adminController.admin);
+// router.get('/test', audioBookAdmin);
 
 module.exports = router
